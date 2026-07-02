c35d7f2b515745f0c305386a46312eb5f256ca90
Mike Georgeff <mike@georgeff.co>
2026-07-02 13:32:19 -0500

    add container build

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,21 @@
 { pkgs, ... }: {
   packages = [ pkgs.go ];
+  containers.http-destination = {
+    name = "http-destination";
+    entrypoint = [ "/bin/worker" ];
+    copyToRoot = pkgs.buildEnv {
+      name = "http-destination-root";
+      paths = [
+        pkgs.cacert
+        (pkgs.runCommand "worker-bin" { } ''
+          mkdir -p $out/bin
+          cp ${./worker} $out/bin/worker
+          chmod +x $out/bin/worker
+        '')
+      ];
+    };
+  };
+
   enterShell = ''
     set +x
     set -a; [ -f .env ] && source .env; set +a
